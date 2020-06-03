f9d6ffee442bac61c22d799de68e31896ade47a6
Rashuoo7 <rashtradeeptripathi@gmail.com>
2020-06-03 10:59:38 +0530

    initial

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Blogger-Boilerplate-Template
-This is an open source boilerplate template for blogger.com.
+# blogger-boilerplate
+A custom boilerplate to develop custom blogger templates using pug and scss modules.
+
+# How to install
+Just download or clone this repo and install dependencies.
+```bash
+npm install
+```
+**pnpm**
+```bash
+pnpm install
+```
+# What can you do?
+Using this boilerplate allows you to use custom **pug** and **sass** modules.
+
+![alt text](./docs/folder-structure.png "Folder Structure")
+
+This way you can create scalable templates with the benefits of these 2 languages.
+
+## Compile
+Just run the following.
+```sh
+gulp || npm start
+```
+You can compile just the template.
+```sh
+npm run pug
+```
+Styles: `npm run sass`
+
+JavaScript: `npm run babel`
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+// Modules
+const gulp = require('gulp'),
+  pug = require('gulp-pug'),
+  sass = require('gulp-sass'),
+  babel = require('gulp-babel'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require("gulp-rename"),
+  plumber = require('gulp-plumber'),
+  prefixer = require('gulp-autoprefixer'),
+
+  // Config
+  options = {
+    babel: {
+      presets: ['@babel/env']
+    },
+    prefixer: {
+      versions: ['last 2 browsers']
+    },
+    pug: {
+      pretty: false
+    },
+    sass: {
+      outputStyle: 'compressed'
+    },
+    paths: {
+      pug: `./src/pug/`,
+      sass: `./src/scss`,
+      es6: `./src/es6`,
+      css: `./src/pug/assets/css`,
+      js: `./src/pug/assets/js`,
+      html: `./dist/`
+    }
+  }
+
+// Tasks
+gulp.task('pug', () =>
+  gulp
+  .src(`${options.paths.pug}/index.pug`) // Only compile index
+  .pipe(plumber())
+  .pipe(pug(options.pug))
+  .pipe(rename('template.xml'))
+  .pipe(gulp.dest(options.paths.html))
+)
+
+gulp.task('sass', () =>
+  gulp
+  .src(`${options.paths.sass}/**/*.scss`)
+  .pipe(plumber())
+  .pipe(sass(options.sass))
+  .pipe(prefixer(options.prefixer))
+  .pipe(gulp.dest(options.paths.css))
+)
+
+gulp.task('babel', () =>
+  gulp
+  .src(`${options.paths.es6}/**/*.js`)
+  .pipe(plumber())
+  .pipe(concat('main.js'))
+  .pipe(babel(options.babel))
+  .pipe(uglify())
+  .pipe(gulp.dest(options.paths.js))
+)
+
+// Watchers
+gulp.task('default', () => {
+  gulp.watch(`${options.paths.pug}/**/*.pug`, ['pug'])
+  gulp.watch(`${options.paths.sass}/**/*.scss`, ['sass'])
+  gulp.watch(`${options.paths.es6}/**/*.js`, ['babel'])
+})
